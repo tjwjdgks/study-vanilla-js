c757fafad662a25ed456daf244e0edbf9005264a
tjwjdgks <tjwjdgks43@gmail.com>
2022-07-25 14:15:25 +0900

    step 7

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASS = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username  = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASS);
+    greeting.classList.remove(HIDDEN_CLASS);
+    greeting.innerText  = username;
 }
 
-function handlerLinkClick(){
-    alert("clicked");
+function handlerLinkClick(event){
+    event.preventDefault();
+    console.log(event);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit)
