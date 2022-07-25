208958d6b07716a5dc266b41994cd742d5545de1
tjwjdgks <tjwjdgks43@gmail.com>
2022-07-25 14:07:19 +0900

    step 6 prevent default

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(loginInput.value);
 }
 
+function handlerLinkClick(){
+    alert("clicked");
+}
+
 loginForm.addEventListener("submit",onLoginSubmit)
+
+link.addEventListener("click",handlerLinkClick);
