efaa0a9ffaeab8c72d024e49c29b9eacf9508f72
tjwjdgks <tjwjdgks43@gmail.com>
2022-07-25 12:02:06 +0900

    step 4 toggle

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const hello = document.querySelector('.hello h1');
 console.log(hello);
 function handleTitleClick(){
     const clickedClass = "active";
-    if(this.classList.contains(clickedClass)){
-        this.classList.remove(clickedClass);
-    }
-    else{ 
-        this.classList.add(clickedClass);
-    }
+    this.classList.toggle(clickedClass);
 }
 
 hello.addEventListener('click',handleTitleClick);
