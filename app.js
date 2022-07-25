b2e9b4b876d92ae834e2bcf7382145dfe0729392
tjwjdgks <tjwjdgks43@gmail.com>
2022-07-25 14:34:38 +0900

    step 8 function with local storage

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     const username  = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASS);
-    greeting.classList.remove(HIDDEN_CLASS);
-    greeting.innerText  = username;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username);
 }
 
 function handlerLinkClick(event){
@@ -18,6 +19,20 @@ function handlerLinkClick(event){
     console.log(event);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit)
+function paintGreeting(savedUserName){
+    greeting.classList.remove("hidden");
+    greeting.innerText = `${savedUserName}`;
+}
 
 link.addEventListener("click",handlerLinkClick);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit",onLoginSubmit)
+
+}
+else{
+   paintGreeting(savedUserName);
+}
