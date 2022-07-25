b4c7a2b539268f910ebbe4e7bdd5703366e5016a
tjwjdgks <tjwjdgks43@gmail.com>
2022-07-25 16:28:23 +0900

    step 10 clock with padstart

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
 const clock = document.querySelector('h2#clock');
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2,"0");
+    const min = String(date.getMinutes()).padStart(2,"0");
+    const sec = String(date.getSeconds()).padStart(2,"0");
+
+    clock.innerText = `${hour}:${min}:${sec}`
+
 }
 
-
-setTimeout(sayHello,5000);
+getClock();
+setInterval(getClock,1000);
